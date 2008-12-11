471efad1bbcdf4c3891930256f7f0c4e202a9b24
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-12-11 09:25:12 +0000

    - enlarging short name because ambiguities with Workflow rating long names

---
+++ sql/postgresql/upgrade/upgrade-3.3.1.2.0-3.3.1.2.1.sql
@@ -0,0 +1,5 @@
+-- upgrade-3.3.1.2.0-3.3.1.2.1.sql
+
+SELECT acs_log__debug('/packages/intranet-simple-survey/sql/postgresql/upgrade/upgrade-3.3.1.2.0-3.3.1.2.1.sql','');
+
+alter table survsimp_surveys alter column short_name type varchar(40);
