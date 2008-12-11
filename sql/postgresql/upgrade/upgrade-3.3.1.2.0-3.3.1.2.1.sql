84c66d701cafea841d11493177ec632373f61781
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-12-11 09:25:29 +0000

    - extended to 100 characters

--- sql/postgresql/upgrade/upgrade-3.3.1.2.0-3.3.1.2.1.sql
+++ sql/postgresql/upgrade/upgrade-3.3.1.2.0-3.3.1.2.1.sql
@@ -2,4 +2,4 @@
 
 SELECT acs_log__debug('/packages/intranet-simple-survey/sql/postgresql/upgrade/upgrade-3.3.1.2.0-3.3.1.2.1.sql','');
 
-alter table survsimp_surveys alter column short_name type varchar(40);
+alter table survsimp_surveys alter column short_name type varchar(100);
